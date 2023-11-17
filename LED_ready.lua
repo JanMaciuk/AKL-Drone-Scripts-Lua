c8f4bcd5406aea535dd63ef75c0b16a255eda65d
JanMaciuk <38810823+JanMaciuk@users.noreply.github.com>
2023-11-17 10:52:41 +0100

    Print all checked parameters on ground station

--- LED_ready.lua
+++ LED_ready.lua
@@ -32,65 +32,64 @@ function update()
     local readyForFlight = true -- if any of the checks fail, this will be set to false
 
     --Cant use "variable = condition" instead of "if condition true variable = true", have to only change value on false
+    --Maximum amount of ground station prints limited, have to use these massive prints
 -- Conditions:
     if (battery:voltage(instanceNo) < minBatteryVoltage) then
         readyForFlight = false
-        print("voltage: ")
-        print(battery:voltage(instanceNo))
     end
+    print("\n".."voltage: ".. tostring(battery:voltage(instanceNo)).. tostring(battery:voltage(instanceNo) > minBatteryVoltage).. "\n".. "num_sats: ".. tostring(gps:num_sats(instanceNo)).. tostring(gps:num_sats(instanceNo) > minSateliteNumber).. "\n".. "is_armed: ".. tostring(arming:is_armed()).. "\n".. "get_mode: ".. tostring(vehicle:get_mode()).. tostring(vehicle:get_mode() == desiredModeNumber).. "\n")
+
 
     if (gps:num_sats(instanceNo) < minSateliteNumber) then
         readyForFlight = false
-        print("num_sats: ")
-        print(gps:num_sats(instanceNo))
     end
+    --print("num_sats: ".. tostring(gps:num_sats(instanceNo)).. tostring(gps:num_sats(instanceNo) > minSateliteNumber).. "\n")
 
     if not arming:is_armed() then
         readyForFlight = false
-        print("is_armed: ")
-        print(tostring(arming:is_armed()))
     end
+    --print("is_armed: ".. tostring(arming:is_armed()).. "\n")
+
 
     if not (vehicle:get_mode() == desiredModeNumber) then
         readyForFlight = false
-        print("get_mode: ")
-        print(vehicle:get_mode())
     end
+    --print("get_mode: ".. tostring(vehicle:get_mode()).. tostring(vehicle:get_mode() == desiredModeNumber).. "\n")
+
 
 --Parameters:
     if not (param_ThrottleFailsafe:get() == value_ThrottleFailsafe) then
         readyForFlight = false
-        print("ThrottleFailsafe: ")
-        print(param_ThrottleFailsafe:get())
     end
+    print("\n".."ThrottleFailsafe: ".. tostring(param_ThrottleFailsafe:get()).. tostring(param_ThrottleFailsafe:get() == value_ThrottleFailsafe).. "\n".."CrashCheck: ".. tostring(param_CrashCheck:get()).. tostring(param_CrashCheck:get() == value_CrashCheck).. "\n".."DeadReckon: ".. tostring(param_DeadReckon:get()).. tostring(param_DeadReckon:get() == value_DeadReckon).. "\n".."EKFAction: ".. tostring(param_EKFAction:get()).. tostring(param_EKFAction:get() == value_EKFAction).. "\n".."GroundStationFail: "..tostring(param_GroundStationFail:get()).. tostring(param_GroundStationFail:get() == value_GroundStationFail).. "\n")
+
 
     if not (param_CrashCheck:get() == value_CrashCheck) then
         readyForFlight = false
-        print("CrashCheck: ")
-        print(param_CrashCheck:get())
     end
+    --print("CrashCheck: ".. tostring(param_CrashCheck:get()).. tostring(param_CrashCheck:get() == value_CrashCheck).. "\n")
+
 
     if not (param_DeadReckon:get() == value_DeadReckon) then
         readyForFlight = false
-        print("DeadReckon: ")
-        print(param_DeadReckon:get())
     end
+   -- print("DeadReckon: ".. tostring(param_DeadReckon:get()).. tostring(param_DeadReckon:get() == value_DeadReckon).. "\n")
+
 
     if not (param_EKFAction:get() == value_EKFAction) then
         readyForFlight = false
-        print("EKFAction: ")
-        print(param_EKFAction:get())
     end
+    --print("EKFAction: ".. tostring(param_EKFAction:get()).. tostring(param_EKFAction:get() == value_EKFAction).. "\n")
+
 
     if not (param_GroundStationFail:get() == value_GroundStationFail) then
         readyForFlight = false
-        print("GroundStationFail: ")
-        param_GroundStationFail:get()
     end
+   -- print("GroundStationFail: "..tostring(param_GroundStationFail:get()).. tostring(param_GroundStationFail:get() == value_GroundStationFail).. "\n")
 
 
 --Led control/feedback:
-    print("Ready for flight: " .. tostring(readyForFlight).."\n\n")
+    print("\n".."Ready for flight: " .. tostring(readyForFlight).."\n\n")
     return update, 10000 -- call again in 10 seconds
 end
 
